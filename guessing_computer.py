6664fc4abc725cca3b758dacd39b25d74fcf91a4
dutzsg <66588754+Dutzsg@users.noreply.github.com>
2021-01-17 01:11:43 -0700

    I added comments
    I literally just added comments

--- guessing_computer.py
+++ guessing_computer.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 import random
 
+# This is the part of the code that gets a random integer and then asks the user to guess it
 def guess(x):
+    # The random integer is generated based on the max number the user previously had to input
     random_number = random.randint(1, x)
     user_guess = 0
+    # Here the code checks if the number is the same as the random number
     while user_guess != random_number:
         try:
             user_guess = int(input(f"What do you think is the number between 1 and {max_guess}?: "))
-        except:
+        except TypeError:
             print("Thats not a number...")
+        except:
+            print("SOMETHING WENT WONGGGG!!!!!")
 
         if user_guess > random_number:
             print("The number is too big!")
@@ -17,11 +22,15 @@ def guess(x):
         elif user_guess == random_number:
             print("You got it!")
 
+# This is in case the user inputs something invalid or something else goes wrong
 while True:
     try:
         max_guess = int(input("What should the max guess be?: "))
         break
-    except:
+    except TypeError:
         print("That's not a number!!!")
+    except:
+        print("Something went wrong...")
+
 
 guess(max_guess)
